d4eccdbba1ce5b2d38d1dd54f70917e27a1eafae
manoperalta <manoperalta@gmail.com>
2025-01-18 03:44:11 -0300

    Atualizacoes nos nomes

--- home.js
+++ home.js
@@ -1,6 +1,5 @@
 function mostraNome(nome){
     return nome
 }
-function mostraSobreNome(Sobrenome){
-    return Sobrenome
-}
+function mostraSobreNome(nome, sobrenome){
+    return nome + sobrenome;
